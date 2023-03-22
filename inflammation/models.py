ddfbf58cc7f680f540744c3c8d83a787a413ae85
Tim Neijenhuis <t.neijenhuis@tudelft.nl>
2023-03-22 10:32:03 +0100

    Added new tests

--- inflammation/models.py
+++ inflammation/models.py
@@ -44,3 +44,21 @@ def daily_min(data):
     """
     return np.min(data, axis=0)
 
+
+def patient_normalise(data):
+    """
+    Normalise patient data from a 2D inflammation data array.
+
+    NaN values are ignored, and normalised to 0.
+
+    Negative values are rounded to 0.
+
+    :param data: A 2D array with inflammation data.
+    :returns: A normalized 2D inflammation data array.
+    """
+    max = np.nanmax(data, axis=1)
+    with np.errstate(invalid='ignore', divide='ignore'):
+        normalised = data / max[:, np.newaxis]
+    normalised[np.isnan(normalised)] = 0
+    normalised[normalised < 0] = 0
+    return normalised
